26e26ba15001028abcfc496afefc49d98e0d4749
Shamim Hossain <hossains159@gmail.com>
2025-06-08 16:41:01 +0600

    Day 2 (#1)
    * feat: add CatsController with CRUD operations and CreateCatDto
    
    * feat: implement Users module with CRUD operations and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsController } from './cats/cats.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UsersModule],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
